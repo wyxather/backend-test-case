7852c7f75c1d046b4f96c162fecce35dc4e9836c
wyxather <79754485+wyxather@users.noreply.github.com>
2024-08-03 19:22:42 +0700

    feat: Remove penalty when neccessary.

--- src/borrow/borrow_services.js
+++ src/borrow/borrow_services.js
@@ -1,15 +1,29 @@
 import { update_book } from "../book/book_service.js";
-import { find_member_by_id, update_member } from "../member/member_service.js";
+import {
+  find_member_by_id,
+  update_member,
+  update_member_by_id,
+} from "../member/member_service.js";
 
+// FIXME: Might need to check for race conditions and avoid it.
 export async function borrow_book_by_id(prisma, member_id, book_id) {
   const found_member = await find_member_by_id(prisma, member_id);
   if (found_member == null) {
     throw new Error("Member doesn't exist.");
   }
 
+  if (found_member.penalized_until != null) {
+    const now = new Date();
+    const penalized_until = new Date(found_member.penalized_until);
+    if (now > penalized_until) {
+      await update_member_by_id(prisma, member_id, { penalized_until: null });
+    } else {
+      throw new Error("Member is penalized.");
+    }
+  }
+
   // https://github.com/prisma/prisma/discussions/22974
   // Since the feature is not available we need to check it here.
-  // FIXME: Might need to check for race conditions and avoid it.
   if (found_member.borrowed_books.length >= 2) {
     throw new Error("Member has borrowed 2 books.");
   }
